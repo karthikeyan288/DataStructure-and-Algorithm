e77acd6fe8521dc3def370bacf1b5eae292a2663
“karthikeyan288” <karthikeyanak28@gmail.com>
2021-11-26 05:21:55 +0530

    ADDED : The message demonstration using regex

---
+++ src/com/Message/MessageDetail.java
@@ -0,0 +1,11 @@
+package com.Message;
+
+public class MessageDetail {
+	public static void main(String[] args) {
+		Method method=Method.getInstance();
+		method.inputdata();
+		method.display();
+	
+	}
+
+}
